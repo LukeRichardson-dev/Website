ea5868c8a6558c858d4b1f10dc1f00d6ba102633
lukerichardson-dev <richardsonluke232@gmail.com>
2022-02-26 17:25:48 +0000

    Added funny

--- personal_website/src/App.tsx
+++ personal_website/src/App.tsx
@@ -6,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='https://cdn.discordapp.com/attachments/798316489173106730/947176654780911666/unknown.png' className="App-logo" alt="logo" />
       </header>
     </div>
   );
